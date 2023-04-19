a1e5fe2e04f22b9a22a8176859f8b41889825ec8
Hector Barazorda <ph.barazorda@gmail.com>
2023-04-19 17:19:52 -0500

    🐛 FIX: readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/indent */
-import { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
 import { Table } from './components/Table'
 import type { User } from './interfaces'
